6cba1d115ac12619fd312daac71d8d42492e6d7f
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-02-27 18:27:23 +0100

    Update school contract

--- contracts/school.sol
+++ contracts/school.sol
@@ -9,15 +9,26 @@ contract School {
         uint age;
     }
 
+    struct Module {
+        string name;
+    }
+
     struct Course {
         string name;
         uint fee;
-        address[] students;
         Instructor[] instructors;
+        mapping(uint=> Module) modules;
+        uint lastModule;
+    }
+
+    struct CourseOutput {
+        string name;
+        uint fee;
+        Instructor[] instructors;
+        uint lastModule;
     }
 
     mapping(uint => Course) private courses;
-    mapping(address => Course[]) private myCourses;
     address payable private owner;
     string schoolName;
     uint private currentIndex = 0;
@@ -44,23 +55,39 @@ contract School {
         currentIndex += 1;
     }
 
+    function addModule(string memory _name, uint _cid) external isOwner {
+        Course storage course = courses[_cid];
+        course.modules[course.lastModule] = Module(_name);
+        course.lastModule += 1;
+        currentIndex += 1;
+    }
+
     function addInstructor(string memory _name, uint _age, uint _index) external isOwner {
         Course storage course = courses[_index];
         course.instructors.push(Instructor(_name, _age));
     }
 
-    function enroll(uint _index) external payable isNotOwner{
-        Course storage course = courses[_index];
-        require(msg.value == course.fee * 1 wei, "Insufficient Amount.");
-        course.students.push(msg.sender);
-        myCourses[msg.sender].push(course);
+    function fetchCourses() external view returns(CourseOutput[] memory){
+        CourseOutput[] memory _courses = new CourseOutput[](currentIndex+1);
+        for (uint i=0; i<currentIndex+1; i++){
+            CourseOutput memory c = CourseOutput(courses[i].name, courses[i].fee, courses[i].instructors, courses[i].lastModule);
+            _courses[i] = c;
+        }
+        return _courses;
     }
 
-    function fetchCourse(uint _index) external view returns(Course memory _course){
-        return courses[_index];
+    function fetchCourse(uint index) external view returns(CourseOutput memory){
+        Course storage course = courses[index];
+        CourseOutput memory returnCourse = CourseOutput(course.name, course.fee, course.instructors, course.lastModule);
+        return returnCourse;
     }
 
-    function myCourse() external view returns(Course[] memory _mycourses){
-        return myCourses[msg.sender];
+    function fetchModules(uint _cindex) external view returns(Module[] memory){
+        Course storage course = courses[_cindex];
+        Module[] memory modules = new Module[](course.lastModule+1);
+        for (uint i=0; i<course.lastModule+1; i++){
+            modules[i] = course.modules[i];
+        }
+        return modules;
     }
 }
