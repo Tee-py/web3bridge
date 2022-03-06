624f96bc74a9d810a304702c54d7f1d617215a9b
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-03-06 12:18:25 +0100

    Update school.sol

--- contracts/school.sol
+++ contracts/school.sol
@@ -11,6 +11,7 @@ contract School {
 
     struct Module {
         string name;
+        string description;
     }
 
     struct Course {
@@ -18,20 +19,21 @@ contract School {
         uint fee;
         Instructor[] instructors;
         mapping(uint=> Module) modules;
-        uint lastModule;
+        uint moduleCount;
     }
 
-    struct CourseOutput {
+    struct CourseWithoutMapping {
         string name;
         uint fee;
         Instructor[] instructors;
-        uint lastModule;
+        uint moduleCount;
     }
 
+    string schoolName;
+    uint private courseCount = 0;
     mapping(uint => Course) private courses;
     address payable private owner;
-    string schoolName;
-    uint private currentIndex = 0;
+    
 
     constructor(string memory _name) {
         schoolName = _name;
@@ -49,43 +51,42 @@ contract School {
     }
 
     function createCourse(string memory _name, uint _fee) external isOwner {
-        Course storage newCourse = courses[currentIndex];
+        Course storage newCourse = courses[courseCount];
         newCourse.name = _name;
         newCourse.fee = _fee;
-        currentIndex += 1;
+        courseCount += 1;
     }
 
-    function addModule(string memory _name, uint _cid) external isOwner {
+    function addModule(Module memory _module, uint _cid) external isOwner {
         Course storage course = courses[_cid];
-        course.modules[course.lastModule] = Module(_name);
-        course.lastModule += 1;
-        currentIndex += 1;
+        course.modules[course.moduleCount] = _module;
+        course.moduleCount += 1;
     }
 
-    function addInstructor(string memory _name, uint _age, uint _index) external isOwner {
+    function addInstructor(Instructor memory _instructor, uint _index) external isOwner {
         Course storage course = courses[_index];
-        course.instructors.push(Instructor(_name, _age));
+        course.instructors.push(_instructor);
     }
 
-    function fetchCourses() external view returns(CourseOutput[] memory){
-        CourseOutput[] memory _courses = new CourseOutput[](currentIndex+1);
-        for (uint i=0; i<currentIndex+1; i++){
-            CourseOutput memory c = CourseOutput(courses[i].name, courses[i].fee, courses[i].instructors, courses[i].lastModule);
+    function fetchCourses() external view returns(CourseWithoutMapping[] memory){
+        CourseWithoutMapping[] memory _courses = new CourseWithoutMapping[](courseCount);
+        for (uint i=0; i<courseCount; i++){
+            CourseWithoutMapping memory c = CourseWithoutMapping(courses[i].name, courses[i].fee, courses[i].instructors, courses[i].moduleCount);
             _courses[i] = c;
         }
         return _courses;
     }
 
-    function fetchCourse(uint index) external view returns(CourseOutput memory){
+    function fetchCourse(uint index) external view returns(CourseWithoutMapping memory){
         Course storage course = courses[index];
-        CourseOutput memory returnCourse = CourseOutput(course.name, course.fee, course.instructors, course.lastModule);
+        CourseWithoutMapping memory returnCourse = CourseWithoutMapping(course.name, course.fee, course.instructors, course.moduleCount);
         return returnCourse;
     }
 
     function fetchModules(uint _cindex) external view returns(Module[] memory){
         Course storage course = courses[_cindex];
-        Module[] memory modules = new Module[](course.lastModule+1);
-        for (uint i=0; i<course.lastModule+1; i++){
+        Module[] memory modules = new Module[](course.moduleCount);
+        for (uint i=0; i<course.moduleCount; i++){
             modules[i] = course.modules[i];
         }
         return modules;
